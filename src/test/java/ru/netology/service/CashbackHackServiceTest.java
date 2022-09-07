43fbf83400463087fe168ca40199c755c896b254
Юлия Алексина <uliyaalexina@mail.ru>
2022-09-07 08:21:21 -0700

    Fixed build.gradle

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -28,5 +28,15 @@ public class CashbackHackServiceTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    void calculateCashback1000() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
 }
 
